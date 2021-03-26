50ac9122d87c6d220ab5166649673d96c123ade2
ebinabo <edgarjohn95@gmail.com>
2021-03-26 21:47:06 +0200

    chore: set up structure

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    purge: ["./styles/**/*.{tsx}"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            color: {
+                blue: {
+                    100: "#EDF4F9", //light blue
+                    300: "#76819E", // text blue
+                    500: "#0078D2", // primary blue
+                    700: "#0061AB", // second blue
+                },
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
